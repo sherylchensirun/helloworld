a9cdaf467ca64f5581ea268d9b038fd001196772
sheryl <e0200840@u.nus.edu>
2019-09-17 14:23:12 +0200

    third commit: added homework

--- hello.py
+++ hello.py
@@ -1,3 +1,20 @@
-print("hello")
+#qns 1
+my_name = "name"
+print("Hello name !")
 
-print("bye")
+#qns 2
+a = 10
+b = 4
+rounded_up_division = int((a / b) + 0.5)
+print(rounded_up_division)
+
+#qns 3
+circle_radius = 10
+circle_surface_area = 3.142*(circle_radius**2)
+print(circle_surface_area)
+
+#qns 4
+a = 7
+b = 6
+c = 4
+print("the result of: a + b -c is", eval("a + b - c"))
